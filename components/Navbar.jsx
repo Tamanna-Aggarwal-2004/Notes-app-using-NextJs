dc27cf71c0214092d453f06f6f3bd5a2862d12c4
Tamanna <tamannaaggarwal168@gmail.com>
2026-01-07 23:33:34 +0530

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center bg-gray-900 px-8 py-4">
       <Link className="text-white text-2xl font-bold" href="/">
-        NielCoding.
+        Notes App
       </Link>
       <Link
         className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
